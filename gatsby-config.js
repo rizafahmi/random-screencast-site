1186ebaccc2aaa5814fa35c2ef4729a16f0187e0
Riza Fahmi <rizafahmi@gmail.com>
2017-08-25 14:33:56 +0700

    Add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `GatbyJS Blog`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    }
+  ]
 }
