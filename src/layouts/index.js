f10e4d26e18db1306ce480d56322f08780981b73
Riza Fahmi <rizafahmi@gmail.com>
2017-09-25 09:59:07 +0700

    fixing static file error

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,18 +10,18 @@ const Header = () => (
   <div className='grid'>
     <header className='grid__col-12 panel--padded--centered'>
       <a className='site-logo' href='/'>
-        <img src='static/logo.svg' />
+        <img src='/static/logo.svg' />
       </a>
       <nav className='navbar' role='navigation'>
         <ul className='nav'>
           <li className='nav__item'>
             <a href='/'>
-              <img className='nav__image' src='static/rss.svg' />
+              <img className='nav__image' src='/static/rss.svg' />
             </a>
           </li>
           <li className='nav__item'>
             <a href='https://github.com/rizafahmi' target='_blank'>
-              <img className='nav__image' src='static/github.svg' />
+              <img className='nav__image' src='/static/github.svg' />
             </a>
           </li>
           <li className='nav__item'>
@@ -29,22 +29,22 @@ const Header = () => (
               href='https://www.youtube.com/channel/UCHhAlFGFCGgIusQkQIqJLYw'
               target='_blank'
             >
-              <img className='nav__image' src='static/youtube.svg' />
+              <img className='nav__image' src='/static/youtube.svg' />
             </a>
           </li>
           <li className='nav__item'>
             <a href='https://facebook.com/rizafahmi' target='_blank'>
-              <img className='nav__image' src='static/fb.svg' />
+              <img className='nav__image' src='/static/fb.svg' />
             </a>
           </li>
           <li className='nav__item'>
             <a href='https://twitter.com/rizafahmi22' target='_blank'>
-              <img className='nav__image' src='static/twitter.svg' />
+              <img className='nav__image' src='/static/twitter.svg' />
             </a>
           </li>
           <li className='nav__item'>
             <a href='https://www.linkedin.com/in/rizafahmi/' target='_blank'>
-              <img className='nav__image' src='static/linkedin.svg' />
+              <img className='nav__image' src='/static/linkedin.svg' />
             </a>
           </li>
         </ul>
