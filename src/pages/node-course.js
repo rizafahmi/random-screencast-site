39d27e201cec62f44a6523ab354434e8471bce3d
Riza Fahmi <rizafahmi@gmail.com>
2018-01-08 09:59:27 +0700

    Update node course landing page

--- src/pages/node-course.js
+++ src/pages/node-course.js
@@ -47,67 +47,73 @@ const NodeCourse = () => (
         },
         { property: 'og:image:height', content: '628' }
       ]}
-    />
+    >
+      <h2>Belajar Membuat Aplikasi Dengan NodeJS.</h2>
+      <p>
+        Mari belajar tentang platform NodeJS dari dasar dan bersama-sama kita
+        akan belajar membuat aplikasi yang ringan dan cepat.
+      </p>
+      <p>Kita juga akan membangun aplikasi dengan cara yang sedikit berbeda, yang jarang dilakukan tutorial pada umumnya.</p>
 
-    <h2>Belajar Membuat Aplikasi Dengan NodeJS.</h2>
-    <p>
-      Mari belajar tentang platform NodeJS dari dasar dan bersama-sama kita akan
-      belajar membuat aplikasi yang ringan dan cepat.
-    </p>
-
-    <p>
-      {' '}
-      Pada online course ini kita akan belajar tentang:
-      <ul>
-        <li>✅ Apa itu NodeJS</li>
-        <li>✅ Kenapa menggunakan NodeJS</li>
-        <li>✅ Bagaimana cara penggunaan NodeJS</li>
-        <li>✅ NodeJS fundamental</li>
-        <li>✅ Instalasi package dengan NPM</li>
-        <li>✅ Membuat aplikasi sederhana dengan NodeJS</li>
-      </ul>
-    </p>
-    <p>
-      Tertarik?! Yuk daftar cukup dengan email kamu. Informasi selanjutnya akan
-      dikirimkan ke inbox kamu. <strong>Tempat terbatas!</strong>
-      <form
-        action='https://randomscreencast.us16.list-manage.com/subscribe/post'
-        accept-charset='UTF-8'
-        method='post'
-      >
-        <input
-          name='u'
-          value='8265eb5b6276ad47f4668bfa1'
-          data-enpassid='__0'
-          type='hidden'
-        />
-        <input name='id' value='bb8c73699a' data-enpassid='__1' type='hidden' />
-        <div data-dojo-attach-point='formFieldsContainer'>
-          <div id='uniqName_3_0' widgetid='uniqName_3_0'>
-            <input
-              autocapitalize='off'
-              autocorrect='off'
-              name='MERGE0'
-              id='MERGE0'
-              size='25'
-              data-enpassid='__5'
-              type='email'
-              className='form__input'
-            />
-            <div className='invalid-error' />
-          </div>
-        </div>
-        <div className='content__button'>
+      <p>
+        {' '}
+        Pada online course ini kita akan belajar tentang:
+        <ul>
+          <li>✅ Apa itu NodeJS</li>
+          <li>✅ Kenapa menggunakan NodeJS</li>
+          <li>✅ Bagaimana cara penggunaan NodeJS</li>
+          <li>✅ NodeJS fundamental</li>
+          <li>✅ Instalasi package dengan NPM</li>
+          <li>✅ Membuat aplikasi sederhana dengan NodeJS</li>
+        </ul>
+      </p>
+      <p>
+        Tertarik?! Yuk daftar cukup dengan email kamu. Informasi selanjutnya
+        akan dikirimkan ke inbox kamu. <strong>Tempat terbatas!</strong>
+        <form
+          action='https://randomscreencast.us16.list-manage.com/subscribe/post'
+          accept-charset='UTF-8'
+          method='post'
+        >
           <input
-            className='btn--warning'
-            value='Daftar sekarang!'
-            data-dojo-attach-point='submitButton'
-            type='submit'
+            name='u'
+            value='8265eb5b6276ad47f4668bfa1'
+            data-enpassid='__0'
+            type='hidden'
           />
-        </div>
-      </form>
-    </p>
-    <BackButton href='/' title='Back' />
+          <input
+            name='id'
+            value='bb8c73699a'
+            data-enpassid='__1'
+            type='hidden'
+          />
+          <div data-dojo-attach-point='formFieldsContainer'>
+            <div id='uniqName_3_0' widgetid='uniqName_3_0'>
+              <input
+                autocapitalize='off'
+                autocorrect='off'
+                name='MERGE0'
+                id='MERGE0'
+                size='25'
+                data-enpassid='__5'
+                type='email'
+                className='form__input'
+              />
+              <div className='invalid-error' />
+            </div>
+          </div>
+          <div className='content__button'>
+            <input
+              className='btn--warning'
+              value='Daftar sekarang!'
+              data-dojo-attach-point='submitButton'
+              type='submit'
+            />
+          </div>
+        </form>
+      </p>
+      <BackButton href='/' title='Back' />
+    </Helmet>
   </div>
 )
 export default NodeCourse
