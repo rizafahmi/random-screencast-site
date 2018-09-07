697a4255408e0f8278854899edcd663fbcc8b650
Riza Fahmi <rizafahmi@gmail.com>
2018-09-07 15:03:51 +0700

    Update link to images

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import './ShareButtons.css'
+import "./ShareButtons.css";
+import twitter from "../images/twitter.svg";
+import fb from "../images/fb.svg";
+import linkedin from "../images/linkedin.svg";
 
 const ShareButtons = props => (
   <div style={{ marginTop: 64 }}>
     <span
       style={{
         fontSize: 18,
-        color: '#999da1',
+        color: "#999da1",
         fontWeight: 300
       }}
     >
@@ -15,44 +18,44 @@ const ShareButtons = props => (
     </span>
     <br />
     <a
-      className='shareLink'
+      className="shareLink"
       href={
-        'https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Frandomscreencast.com' +
+        "https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Frandomscreencast.com" +
         encodeURI(props.url)
       }
-      target='_blank'
-      ariaLabel='Share on Facebook'
+      target="_blank"
+      ariaLabel="Share on Facebook"
     >
-      <img src='/static/fb.svg' style={{ width: 28 }} />
+      <img src={fb} style={{ width: 28 }} />
     </a>
     <a
-      className='shareLink'
+      className="shareLink"
       href={
-        'https://twitter.com/intent/tweet/?text=' +
-        encodeURI(props.title + '&url=https://randomscreencast.com' + props.url)
+        "https://twitter.com/intent/tweet/?text=" +
+        encodeURI(props.title + "&url=https://randomscreencast.com" + props.url)
       }
-      target='_blank'
-      ariaLabel='Share on Twitter'
+      target="_blank"
+      ariaLabel="Share on Twitter"
     >
-      <img src='/static/twitter.svg' style={{ width: 28 }} />
+      <img src="/static/twitter.svg" style={{ width: 28 }} />
     </a>
     <a
-      className='shareLink'
+      className="shareLink"
       href={
-        'https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Frandomscreencast.com' +
+        "https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Frandomscreencast.com" +
         props.url +
-        '&amp;title=' +
+        "&amp;title=" +
         props.title +
-        '&amp;summary=' +
+        "&amp;summary=" +
         props.description +
-        '&amp;source=https%3A%2F%2Frandomscreencast.com'
+        "&amp;source=https%3A%2F%2Frandomscreencast.com"
       }
-      target='_blank'
-      ariaLabel='Share on Linkedin'
+      target="_blank"
+      ariaLabel="Share on Linkedin"
     >
-      <img src='/static/linkedin.svg' style={{ width: 28 }} />
+      <img src="/static/linkedin.svg" style={{ width: 28 }} />
     </a>
   </div>
-)
+);
 
-export default ShareButtons
+export default ShareButtons;
